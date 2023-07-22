6b86a943bcfc775db904f40d7e8bae25dbdf70fb
akiko_kuroda <hahnecochito@gmail.com>
2023-07-22 23:24:51 +0900

    📝 bookのnew/createの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,27 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new # 新規作成（空の家計簿モデルのインスタンス）
+  end
+
+  def create
+    # ストロングパラメータ
+    # リクエストに含まれる全てのパラメータ（POSTデータやurlに含まれるid等）はparamsという変数に格納される。ストロングパラメータを使用すれば、登録に必要なデータだけを取り出せる。
+    book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+
+     # book_paramsには画面から入力したデータが入っている。
+     # それをもとに、Bookモデルを新しくインスタンス化している。ここでデータ入りのBookモデルインスタンスが生成できる。
+    @book = Book.new(book_params)
+    # 持っているデータをdatabaseに保存する
+    if @book.save
+    # 登録後は一覧画面も戻る
+      redirect_to books_path
+    else
+      # renderは表示するファイルを指示するメソッド
+      # バリデーションに引っかかってfalseか返ってきた場合、nweアクションで登録画面を表示する。※newメソッドへ移動するわけではない。
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "books", to: "books#index"
+  get "/books", to: "books#index"
+  post "/books", to: "books#create"
+  # get "/books/new", to: "books#new" # 記å述順を注意。(/books/:idの後に書くと正しくアクションが処理されない ※上かひ比較していくため、idが上だとnewが正しく評価できない)
+  get "/books/new", to: "books#new", as: "new_book"
   get "/books/:id", to: "books#show", as: "book"
 end
