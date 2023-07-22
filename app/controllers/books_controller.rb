0762b007fdec9bf91e524081fb020c9734c5e072
akiko_kuroda <hahnecochito@gmail.com>
2023-07-22 22:31:48 +0900

    💄 📝 bootstrap導入、bookページの一覧・詳細を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
     # ActiveRecordのおかげでdetabaseにアクセスできる
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 end
